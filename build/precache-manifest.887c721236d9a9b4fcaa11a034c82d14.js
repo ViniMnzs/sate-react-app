387a9918fffdad1a9aa97070dfbfaaf4dc4607c6
ViniMnzs <viniribeiro11.000@gmail.com>
2021-02-04 00:15:02 -0300

    build

---
+++ build/precache-manifest.887c721236d9a9b4fcaa11a034c82d14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dcd5427ac73b6d67aa7f70910a35311",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6f4d7a452a93199f4bb",
+    "url": "/static/css/main.380e0d74.chunk.css"
+  },
+  {
+    "revision": "05cd48f343b17fd31c90",
+    "url": "/static/js/2.8dc8d982.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/static/js/2.8dc8d982.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6f4d7a452a93199f4bb",
+    "url": "/static/js/main.3739dbe3.chunk.js"
+  },
+  {
+    "revision": "25fdf5ffee17b1dfb9cf",
+    "url": "/static/js/runtime-main.e46e67e3.js"
+  }
+]);
